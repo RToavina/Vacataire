2d273e803cba6cfa208b30c6ca5f874d9169f8dd
Razanajatovo A. Toavina <razanajatovo.a.toavina@gmail.com>
2022-09-01 10:10:28 +0300

    Login

---
+++ android/app/src/main/java/itu/mbds/vacataire/SplashScreen.java
@@ -0,0 +1,34 @@
+package itu.mbds.vacataire;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView logo;
+    private TextView titre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        logo = (ImageView) findViewById(R.id.logo);
+        titre = (TextView) findViewById(R.id.titre);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        },2000);
+    }
+}

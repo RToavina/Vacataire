4ed8fbbc2bf2f32e88ea76f4e141f2ed5e6c9dac
Razanajatovo A. Toavina <razanajatovo.a.toavina@gmail.com>
2022-09-06 17:08:26 +0300

    First commit web app

---
+++ web/vacataire/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes:Routes = [
+  {
+    path:"login",
+    component:LoginComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

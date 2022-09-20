c6969ccd12217d4ba2fa1aa64e583911bca00f4f
Razanajatovo A. Toavina <razanajatovo.a.toavina@gmail.com>
2022-09-20 16:58:49 +0300

    fab button + emargement

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {RouterModule, Routes} from "@angular/router";
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { EmargementComponent } from './emargement/emargement.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './auth/login/login.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {EmargementComponent} from './emargement/emargement.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import {MatNativeDateModule} from '@angular/material/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { CalendarComponent } from './calendar/calendar.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
@@ -40,10 +40,12 @@ import { CalendarComponent } from './calendar/calendar.component';
     MatNativeDateModule,
     MatMenuModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDialogModule
   ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- web/src/app/calendar/calendar.component.ts
+++ web/src/app/calendar/calendar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarOptions, defineFullCalendarElement } from '@fullcalendar/web-component';
+import {Component, OnInit} from '@angular/core';
+import {CalendarOptions, defineFullCalendarElement} from '@fullcalendar/web-component';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import frLocale from '@fullcalendar/core/locales/fr';
 import interactionPlugin from '@fullcalendar/interaction';
+import {MatDialog} from "@angular/material/dialog";
+import {EmargementComponent} from "../emargement/emargement.component";
 
 // make the <full-calendar> element globally available by calling this function at the top-level
 defineFullCalendarElement();
@@ -14,51 +15,61 @@ defineFullCalendarElement();
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog : MatDialog) {
+  }
 
   ngOnInit(): void {
   }
 
   calendarOptions: CalendarOptions = {
-    locale:'fr',
-    plugins: [dayGridPlugin, interactionPlugin ],
+    locale: 'fr',
+    plugins: [dayGridPlugin, interactionPlugin],
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,dayGridWeek,dayGridDay'
     },
-    buttonText:{
-      today:    "Aujourd'hui",
-      month:    'Mois',
-      week:     'Semaine',
-      day:      'Jour',
-      list:     'Liste'
+    buttonText: {
+      today: "Aujourd'hui",
+      month: 'Mois',
+      week: 'Semaine',
+      day: 'Jour',
+      list: 'Liste'
     },
-    firstDay:1,
-    hiddenDays:[0],
+    firstDay: 1,
+    hiddenDays: [0],
     dateClick: this.emarger,
-    eventDisplay:'auto',
+    eventDisplay: 'auto',
     events: [
       {
-        title  : 'event1',
-        start  : '2022-09-23'
+        title: 'event1',
+        start: '2022-09-23'
       },
       {
-        title  : 'event2',
-        start  : '2022-09-25',
-        end    : '2022-09-27'
+        title: 'event2',
+        start: '2022-09-25',
+        end: '2022-09-27'
       },
       {
-        title  : 'event3',
-        start  : '2022-09-29',
-        allDay : false // will make the time show
+        title: 'event3',
+        start: '2022-09-29',
+        allDay: false // will make the time show
       }
     ],
     height: "auto"
   };
 
-  emarger(info:any){
+  emarger(info: any) {
     //TODO Mettre le corps
   }
 
+  openDialog(): void {
+    const diaglogRef = this.dialog.open(EmargementComponent,{
+      width : '500px',
+    });
+
+    diaglogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
